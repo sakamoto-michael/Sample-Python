f3db59a86c42410c8368620f78957e1a68bab56d
sakamoto-michael <sakamotomichael@hotmail.com>
2020-03-30 11:06:49 +0900

    Initial Commit

---
+++ new/new.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import math
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+def customGreet(name):
+    custom_greeting = "Hello, {}!".format(name)
+    return custom_greeting
+
+print(greet("World"))
+name = input('What is your name? \n')
+print(customGreet(name))
+
+
