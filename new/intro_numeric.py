a86acd53c27bfbe9f870fb59546634c4dcfe4d80
Michael Sakamoto <sakamotomichael@hotmail.com>
2020-03-31 16:19:42 +0900

    Basics of python numeric data

---
+++ new/intro_numeric.py
@@ -0,0 +1,56 @@
+# Python Numeric Data
+
+# Integer VS Float
+num = 3 # Integer
+num2 = 3.14 # Float
+print(type(num))
+print(type(num2))
+
+# Arithmetic operators
+# Addition: 3 + 2
+# Subtraction: 3 - 2
+# Multiplication: 3 * 2
+# Division: 3 / 2
+# Floor Division: 3 // 2
+# Exponent: 3 ** 2
+# Mod: 3 % 2
+
+# Order of operations
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+
+# Incrementing
+num = 1
+num += 1
+print(num)
+num2 = 1
+num2 *= 10
+print(num2)
+
+# Absolute values
+print(abs(-3))
+
+# Rounding to nearest INT value
+print(round(3.75))
+
+# Rounding to first digit AFTER decimal
+print(round(3.75, 1))
+
+# Comparisons
+# Equal: 3 == 2
+# Not Equal: 3 != 2
+# Greater than: 3 > 2
+# Less than: 3 < 2
+# Greater than or equal: 3 >= 2
+# Less than or equal: 3 <= 2
+print(3 <= 2)
+
+# Numbers as strings
+num_1 = '100'
+num_2 = '200'
+print(num_1 + num_2)
+
+# Casting STR to INT
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1 + num_2)
